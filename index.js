e865f166224c008ef2cf4ffd745a9517642e421b
danny <danny@setel.com>
2022-02-22 11:02:12 +0000

    chore: remove comments

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 'use strict';
 const {http} = require('./dist/main.js');
-// const express = require('express')
 
-// const app = express();
-// app.get('/', (req,res) => res.send("Homepage"))
-
-// exports.http = app
 exports.http = http
 
